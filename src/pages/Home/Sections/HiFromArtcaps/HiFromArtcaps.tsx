581a93bb1f35dca57cc25ae75de279fe8e9735bc
RoGaTT <firestyl@yandex.ru>
2021-09-28 04:30:49 +0300

    update chip at hi from artcaps

--- src/pages/Home/Sections/HiFromArtcaps/HiFromArtcaps.tsx
+++ src/pages/Home/Sections/HiFromArtcaps/HiFromArtcaps.tsx
@@ -20,38 +20,14 @@ import ArtCapsChipSetImg from '@/assets/img/hi_from_artcaps/chip_set.png';
 import ArtCapsChipSetShortImg from '@/assets/img/hi_from_artcaps/chip_set_short.svg';
 import HiFromArtCapsChip1Img from '@/assets/img/chips/hi_from_artcaps_1.svg';
 
-import CollectionsChipImg_1_1 from '@/assets/img/chips/1/1.png';
-import CollectionsChipImg_1_2 from '@/assets/img/chips/1/2.png';
-import CollectionsChipImg_1_3 from '@/assets/img/chips/1/3.png';
 import CollectionsChipImg_1_4 from '@/assets/img/chips/1/4.png';
-import CollectionsChipImg_1_5 from '@/assets/img/chips/1/5.png';
-import CollectionsChipImg_2_1 from '@/assets/img/chips/2/1.png';
-import CollectionsChipImg_2_2 from '@/assets/img/chips/2/2.png';
-import CollectionsChipImg_2_3 from '@/assets/img/chips/2/3.png';
 import CollectionsChipImg_2_4 from '@/assets/img/chips/2/4.png';
-import CollectionsChipImg_2_5 from '@/assets/img/chips/2/5.png';
-import CollectionsChipImg_3_1 from '@/assets/img/chips/3/1.png';
-import CollectionsChipImg_3_2 from '@/assets/img/chips/3/2.png';
-import CollectionsChipImg_3_3 from '@/assets/img/chips/3/3.png';
 import CollectionsChipImg_3_4 from '@/assets/img/chips/3/4.png';
-import CollectionsChipImg_3_5 from '@/assets/img/chips/3/5.png';
 
 const CHIP_LIST = [
-  CollectionsChipImg_1_1,
-  CollectionsChipImg_1_2,
-  CollectionsChipImg_1_3,
   CollectionsChipImg_1_4,
-  CollectionsChipImg_1_5,
-  CollectionsChipImg_2_1,
-  CollectionsChipImg_2_2,
-  CollectionsChipImg_2_3,
   CollectionsChipImg_2_4,
-  CollectionsChipImg_2_5,
-  CollectionsChipImg_3_1,
-  CollectionsChipImg_3_2,
-  CollectionsChipImg_3_3,
   CollectionsChipImg_3_4,
-  CollectionsChipImg_3_5,
 ];
 
 interface PropsType {
