128a73caebe133fd06a968d9cc8e546cb9aa6218
RoGaTT <firestyl@yandex.ru>
2021-10-13 22:47:48 +0300

    update teimer

--- src/const/time.ts
+++ src/const/time.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const RELEASE_DATE = new Date(2021, 10, 1, 21, 0, 0, 0);
+export const RELEASE_DATE = new Date(2021, 10, 2, 21, 0, 0, 0);
