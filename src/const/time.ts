0b79c026e28b64484d08c6de6e320cf011a28724
RoGaTT <firestyl@yandex.ru>
2021-10-26 22:42:23 +0300

    update time

--- src/const/time.ts
+++ src/const/time.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const RELEASE_DATE = (() => {
-  const buf = new Date(2021, 10, 1, 21, 0, 0, 0);
+  const buf = new Date(2021, 10, 14, 21, 0, 0, 0);
   // buf.setHours(buf.getHours() + 3);
   return buf;
 })();
